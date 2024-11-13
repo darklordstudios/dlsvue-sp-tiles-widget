b6887c2d3ad2668fb4472b132d9e8859f703b95c
spevilgenius <spevilgenius@verizon.net>
2024-11-13 06:59:52 -0500

    Updates-11-13-24-0700

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { ImagePicker } from 'dls-vue'
+
+const tileswidget = ref(null)
+
+onMounted(() => {
+  console.log('MOUNTED')
+})
+</script>
+
+<template>
+  <div ref="tileswidget"><ImagePicker />></div>
+</template>
+
+<style scoped></style>
